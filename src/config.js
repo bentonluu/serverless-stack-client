0e2961bfcf24d71000eeb700913a9642dd086954
benton.luu <benton.luu@ucalgary.ca>
2021-07-12 14:51:27 -0600

    Building our React app

--- src/config.js
+++ src/config.js
@@ -1,17 +1,18 @@
 const config = {
+  MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
     REGION: "us-east-1",
     BUCKET: "benton-notes-app",
   },
   apiGateway: {
-    REGION: "us-east-1",
     URL: "https://tqj38ik1ja.execute-api.us-east-1.amazonaws.com/prod",
+    REGION: "us-east-1",
   },
   cognito: {
     REGION: "us-east-1",
     USER_POOL_ID: "us-east-1_Gi2giA5P1",
     APP_CLIENT_ID: "4s8rbdi7gpbud5n2hkarosfgs0",
-    IDENTITY_POOL_ID: "us-east-1:c7f82e5b-ebda-4f97-ae4e-6b1be888c418",
+    IDENTITY_POOL_ID: "us-east-1:8f54444a-730f-4a89-9029-37e39db3ae9a",
   },
 };
 
